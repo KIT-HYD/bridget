9fadac6a1f66de0f1e1eb5bbc3b94880d0b6fad2
AlexDo1 <alex.dolich@gmail.com>
2020-04-29 15:55:45 +0200

    Update read.py

--- bridget/io/read.py
+++ bridget/io/read.py
@@ -22,7 +22,7 @@ def read_TK2_file(fname, strict=True, na_values=-9999.9003906):
         Additional values to recognize as NA/NaN.
     """
     read_kwargs = dict(na_values = na_values)
-    return _reader(fname, 'TK2', strict=strict, **read_kwargs)
+    return _reader(fname, 'TK2', strict=strict, read_kwargs=read_kwargs)
 
 
 def read_QA_QC_file(fname, strict=True):
@@ -44,9 +44,9 @@ def read_QA_QC_file(fname, strict=True):
     return _reader(fname, 'QA_QC', strict=strict)
 
 
-def _reader(fname, type_str, strict=True, **read_kwargs):
+def _reader(fname, type_str, strict=True, read_kwargs={}):
     # read the file
-    data = pd.read_csv(fname, sep=',', header=None, index_col=None, na_values=[read_kwargs['na_values']])
+    data = pd.read_csv(fname, sep=',', header=None, index_col=None, **read_kwargs)
     # drop empty columns
     data.dropna(axis=1, how='all', inplace=True)
 
