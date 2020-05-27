e2c2480a968d627b194c9788829bc2c86b797c3c
Mirko Mälicke <mirko.maelicke@kit.edu>
2020-05-27 19:12:54 +0200

    footprint Layout

---
+++ bridget/eddy/footprint.py
@@ -0,0 +1,18 @@
+"""
+The `footprint` submodule implements common methods to calculate the 
+footprint of the measurement from Eddy Covariance data.
+
+Beside several methods that can calculate the timevariat footprint,
+an aggregation method for defining a common footprint region is 
+given.
+
+Examples
+--------
+
+.. note::
+
+    More to come soon
+
+"""
+
+
