867920e684210983425285c5780775b1f5acbbe1
sihassl <38761852+sihassl@users.noreply.github.com>
2021-08-10 13:56:43 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'BridET'
+project = 'BridgET'
 copyright = '2021, bridget contributors'
 author = 'bridget contributors'
 
